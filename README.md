c2dc54a79db3c75d876f5a5a260c2e7196f76c9a
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2022-04-21 13:58:37 +0900

    feat eslintのえらー修正

--- README.md
+++ README.md
@@ -7,3 +7,7 @@
 # 参考記事
 
 https://zenn.dev/rabbit/articles/d7175e62dc56a5
+
+## 型に関して
+
+https://zuma-lab.com/posts/next-eslint-prettier-settings
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,13 @@
 /** @format */
 
-import type { NextPage } from "next";
-import Head from "next/head";
-import Layout from "@/components/layout";
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Layout from '@/components/layout';
 const Index: NextPage = () => {
   return (
     <div>
       <Layout>
-        <h1 className="text-3xl kiwi-maru mt-20 ml-20">
-          コトハジメ、しませんか？
-        </h1>
+        <h1 className='text-3xl kiwi-maru mt-20 ml-20'>コトハジメ、しませんか？</h1>
       </Layout>
     </div>
   );
