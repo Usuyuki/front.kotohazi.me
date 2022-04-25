590a20ee36b323d282cbe580ab431d7f6a44f76d
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2022-04-25 09:24:54 +0900

    wip リリースノート表示だけ

--- src/lib/$path.ts
+++ src/lib/$path.ts
@@ -1,38 +1,38 @@
 export const pagesPath = {
-  $404: {
-    $url: (url?: { hash?: string }) => ({ pathname: '/404' as const, hash: url?.hash }),
+  "$404": {
+    $url: (url?: { hash?: string }) => ({ pathname: '/404' as const, hash: url?.hash })
   },
-  announcement: {
-    $url: (url?: { hash?: string }) => ({ pathname: '/announcement' as const, hash: url?.hash }),
+  "announcement": {
+    $url: (url?: { hash?: string }) => ({ pathname: '/announcement' as const, hash: url?.hash })
   },
-  callback: {
-    $url: (url?: { hash?: string }) => ({ pathname: '/callback' as const, hash: url?.hash }),
+  "callback": {
+    $url: (url?: { hash?: string }) => ({ pathname: '/callback' as const, hash: url?.hash })
   },
-  home: {
-    $url: (url?: { hash?: string }) => ({ pathname: '/home' as const, hash: url?.hash }),
+  "contact": {
+    $url: (url?: { hash?: string }) => ({ pathname: '/contact' as const, hash: url?.hash })
   },
-  contact: {
-    $url: (url?: { hash?: string }) => ({ pathname: '/contact' as const, hash: url?.hash }),
+  "home": {
+    $url: (url?: { hash?: string }) => ({ pathname: '/home' as const, hash: url?.hash })
   },
-  license: {
-    $url: (url?: { hash?: string }) => ({ pathname: '/license' as const, hash: url?.hash }),
+  "license": {
+    $url: (url?: { hash?: string }) => ({ pathname: '/license' as const, hash: url?.hash })
   },
-  login: {
-    $url: (url?: { hash?: string }) => ({ pathname: '/login' as const, hash: url?.hash }),
+  "login": {
+    $url: (url?: { hash?: string }) => ({ pathname: '/login' as const, hash: url?.hash })
   },
-  privacyPolicy: {
-    $url: (url?: { hash?: string }) => ({ pathname: '/privacyPolicy' as const, hash: url?.hash }),
+  "privacyPolicy": {
+    $url: (url?: { hash?: string }) => ({ pathname: '/privacyPolicy' as const, hash: url?.hash })
   },
-  releaseNote: {
-    $url: (url?: { hash?: string }) => ({ pathname: '/releaseNote' as const, hash: url?.hash }),
+  "releaseNote": {
+    $url: (url?: { hash?: string }) => ({ pathname: '/releaseNote' as const, hash: url?.hash })
   },
-  terms: {
-    $url: (url?: { hash?: string }) => ({ pathname: '/terms' as const, hash: url?.hash }),
+  "terms": {
+    $url: (url?: { hash?: string }) => ({ pathname: '/terms' as const, hash: url?.hash })
   },
-  whitespace: {
-    $url: (url?: { hash?: string }) => ({ pathname: '/whitespace' as const, hash: url?.hash }),
+  "whitespace": {
+    $url: (url?: { hash?: string }) => ({ pathname: '/whitespace' as const, hash: url?.hash })
   },
-  $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash }),
-};
+  $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash })
+}
 
-export type PagesPath = typeof pagesPath;
+export type PagesPath = typeof pagesPath
--- src/pages/releaseNote.tsx
+++ src/pages/releaseNote.tsx
@@ -2,7 +2,7 @@
 
 import type { NextPage } from 'next';
 import Layout from '@/components/layouts/VisitorLayout';
-
+import ReleaseNoteSentence from '@/components/sentence/ReleaseNoteSentence';
 export const getStaticProps = async () => {
   const data = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/release-notes/all`).then((r) => r.json());
   return { props: { data }, revalidate: 120 };
@@ -18,7 +18,15 @@ const releaseNote: NextPage = (data) => {
         bgColorClass='bg-low'
         description='コトハジメのリリースノート'
       >
-        <div></div>
+        <div className='flex flex-col justify-center items-center'>
+          {data.data.map((content, key) => {
+            return (
+              <ReleaseNoteSentence key={key} title={content.title} date={content.date} genre={content.genre}>
+                {content.description}
+              </ReleaseNoteSentence>
+            );
+          })}
+        </div>
       </Layout>
     </div>
   );
